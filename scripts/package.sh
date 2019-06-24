b4e583867846cf2661d7113a1345727c3c461f8c
Don Nguyen <dnguyen@pivotal.io>
2019-06-24 17:53:52 -0400

    Add correct scripts
    Co-authored-by: Tilly Taylor <mataylor@pivotal.io>

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
-#go run vendor/github.com/cloudfoundry/cnb-tools/install_tools/main.go "$@"
+set -eo pipefail
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+./scripts/install_tools.sh
 
-go run -mod=vendor "$DIR/cmd_router.go" package "$@"
+PACKAGE_DIR=${PACKAGE_DIR:-"${PWD##*/}_$(openssl rand -hex 4)"}
+
+full_path=$(realpath "$PACKAGE_DIR")
+args=".bin/packager -uncached"
+
+if [[ $1 == "-c" ]] || [[ $2 == "-c" ]]; then #package as cached
+    full_path="$full_path-cached"
+    args=".bin/packager"
+fi
+
+if [[ $1 == "-a" ]] || [[ $2 == "-a" ]]; then #package as archive
+    args="${args} -archive"
+fi
+eval "$args $full_path"
+
+if [[ -n "$BP_REWRITE_HOST" ]]; then
+    sed -i '' -e "s|^uri = \"https:\/\/buildpacks\.cloudfoundry\.org\(.*\)\"$|uri = \"http://$BP_REWRITE_HOST\1\"|g" "$full_path/buildpack.toml"
+fi
