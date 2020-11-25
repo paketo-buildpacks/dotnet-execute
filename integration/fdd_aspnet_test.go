20570cf4851ba33ea16093658694507c1732325e
Sophie Wigmore <swigmore@vmware.com>
2020-11-25 19:30:02 +0000

    Look for built app from dotnet publish in /layers dir
    Signed-off-by: Frankie Gallina-Jones <frankieg@vmware.com>

--- integration/fdd_aspnet_test.go
+++ integration/fdd_aspnet_test.go
@@ -85,7 +85,7 @@ func testFddASPNet(t *testing.T, context spec.G, it spec.S) {
 			Expect(logs).To(ContainLines(
 				MatchRegexp(fmt.Sprintf(`%s \d+\.\d+\.\d+`, settings.BuildpackInfo.Name)),
 				"  Assigning launch processes",
-				`    web: dotnet simple_aspnet_app.dll --urls http://0.0.0.0:${PORT:-8080}`,
+				`    web: dotnet /workspace/simple_aspnet_app.dll --urls http://0.0.0.0:${PORT:-8080}`,
 				"",
 			))
 		})
--- integration/fde_aspnet_test.go
+++ integration/fde_aspnet_test.go
@@ -85,7 +85,7 @@ func testFdeASPNet(t *testing.T, context spec.G, it spec.S) {
 			Expect(logs).To(ContainLines(
 				MatchRegexp(fmt.Sprintf(`%s \d+\.\d+\.\d+`, settings.BuildpackInfo.Name)),
 				"  Assigning launch processes",
-				`    web: ./simple_aspnet_app --urls http://0.0.0.0:${PORT:-8080}`,
+				`    web: /workspace/simple_aspnet_app --urls http://0.0.0.0:${PORT:-8080}`,
 				"",
 			))
 		})
