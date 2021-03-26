cf363334991d67d3c88896b778838a34490219de
Timothy Hitchener <thitchener@pivotal.io>
2021-03-26 13:43:40 +0000

    Run unit tests sequentially
    Signed-off-by: Emily Johnson <emjohnson@vmware.com>

--- init_test.go
+++ init_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestUnitDotnetExecute(t *testing.T) {
-	suite := spec.New("dotnet-execute", spec.Report(report.Terminal{}), spec.Parallel())
+	suite := spec.New("dotnet-execute", spec.Report(report.Terminal{}), spec.Sequential())
 	suite("Build", testBuild)
 	suite("BuildpackYMLParser", testBuildpackYMLParser)
 	suite("Detect", testDetect)
