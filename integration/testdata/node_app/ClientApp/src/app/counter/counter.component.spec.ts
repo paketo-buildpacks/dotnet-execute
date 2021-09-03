c31b85b59b23ae94c94d0a745619e4fd3cd825a8
Frankie Gallina-Jones <frankieg@vmware.com>
2021-09-03 17:22:28 -0400

    Revert "replace node app with .NET 5 angular sample"
    the generated app did not work with the buildpack.
    
    This reverts commit f597bae7a0a9afdcab5ac409c8fbd90c390f28b2.

--- integration/testdata/node_app/ClientApp/src/app/counter/counter.component.spec.ts
+++ integration/testdata/node_app/ClientApp/src/app/counter/counter.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CounterComponent } from './counter.component';
 
 describe('CounterComponent', () => {
+  let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
 
   beforeEach(async(() => {
@@ -14,6 +15,7 @@ describe('CounterComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
