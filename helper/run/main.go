80e7a5416b5c21ae16509a1e1aede03b010c7ebc
Joshua T. Casey <joshuatcasey@gmail.com>
2022-03-09 14:02:53 -0600

    Don't use libpak

--- helper/run/main.go
+++ helper/run/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/paketo-buildpacks/dotnet-execute/helper"
-	"github.com/paketo-buildpacks/libpak/sherpa"
+	"os"
+	"strconv"
 )
 
 func main() {
-	sherpa.Execute(func() error {
-		return sherpa.Helpers(map[string]sherpa.ExecD{
-			`helper`: helper.Helper{},
-		})
-	})
+	execdWriter := os.NewFile(3, "/dev/fd/3")
+
+	envVars, err := helper.Execute()
+	if err != nil {
+		return
+	}
+
+	for k, v := range envVars {
+		if _, err := fmt.Fprintf(execdWriter, "%s=%s\n", k, strconv.Quote(v)); err != nil {
+			return
+		}
+	}
 }
