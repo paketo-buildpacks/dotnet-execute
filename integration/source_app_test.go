e1ce9eb4db7a30b5c5a6268371636b2f6f935685
Frankie Gallina-Jones <frankieg@vmware.com>
2022-07-13 12:58:44 -0400

    re-add source code tests

---
+++ integration/source_app_test.go
@@ -0,0 +1,118 @@
+package integration_test
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/paketo-buildpacks/occam"
+	"github.com/sclevine/spec"
+
+	. "github.com/onsi/gomega"
+	. "github.com/paketo-buildpacks/occam/matchers"
+)
+
+func testSourceApp(t *testing.T, context spec.G, it spec.S) {
+	var (
+		Expect     = NewWithT(t).Expect
+		Eventually = NewWithT(t).Eventually
+
+		pack   occam.Pack
+		docker occam.Docker
+	)
+
+	it.Before(func() {
+		pack = occam.NewPack().WithVerbose().WithNoColor()
+		docker = occam.NewDocker()
+	})
+
+	context("when building a default source app", func() {
+		var (
+			image     occam.Image
+			container occam.Container
+
+			name   string
+			source string
+		)
+
+		it.Before(func() {
+			var err error
+			name, err = occam.RandomName()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		it.After(func() {
+			Expect(docker.Container.Remove.Execute(container.ID)).To(Succeed())
+			Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
+			Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
+			Expect(os.RemoveAll(source)).To(Succeed())
+		})
+
+		it("builds and runs successfully", func() {
+			var err error
+			source, err = occam.Source(filepath.Join("testdata", "source_app"))
+			Expect(err).NotTo(HaveOccurred())
+
+			var logs fmt.Stringer
+			image, logs, err = pack.Build.
+				WithPullPolicy("never").
+				WithBuildpacks(
+					settings.Buildpacks.ICU.Online,
+					settings.Buildpacks.DotnetCoreRuntime.Online,
+					settings.Buildpacks.DotnetCoreASPNet.Online,
+					settings.Buildpacks.DotnetCoreSDK.Online,
+					settings.Buildpacks.DotnetPublish.Online,
+					settings.Buildpacks.DotnetExecute.Online,
+				).
+				Execute(name, source)
+			Expect(err).ToNot(HaveOccurred(), logs.String)
+
+			container, err = docker.Container.Run.
+				WithEnv(map[string]string{"PORT": "8080"}).
+				WithPublish("8080").
+				WithPublishAll().
+				Execute(image.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(container).Should(Serve(ContainSubstring("simple_3_0_app")).OnPort(8080))
+		})
+
+		for _, b := range settings.Config.Builders {
+			var builder string = b
+			context(fmt.Sprintf("with %s builder", builder), func() {
+				context("when 'net6.0' is specified as the TargetFramework", func() {
+					it("builds and runs successfully", func() {
+						var err error
+						source, err = occam.Source(filepath.Join("testdata", "source_6"))
+						Expect(err).NotTo(HaveOccurred())
+
+						var logs fmt.Stringer
+						image, logs, err = pack.Build.
+							WithPullPolicy("never").
+							WithBuildpacks(
+								settings.Buildpacks.ICU.Online,
+								settings.Buildpacks.DotnetCoreRuntime.Online,
+								settings.Buildpacks.DotnetCoreASPNet.Online,
+								settings.Buildpacks.DotnetCoreSDK.Online,
+								settings.Buildpacks.DotnetPublish.Online,
+								settings.Buildpacks.DotnetExecute.Online,
+							).
+							WithBuilder(builder).
+							Execute(name, source)
+						Expect(err).ToNot(HaveOccurred(), logs.String)
+
+						container, err = docker.Container.Run.
+							WithEnv(map[string]string{"PORT": "8080"}).
+							WithPublish("8080").
+							WithPublishAll().
+							Execute(image.ID)
+						Expect(err).NotTo(HaveOccurred())
+
+						Eventually(container).Should(Serve(ContainSubstring("source_6_app")).OnPort(8080))
+					})
+				})
+			})
+		}
+	})
+}
