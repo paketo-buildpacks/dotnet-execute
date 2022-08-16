4fbccbe1fc721216a2172fd6de39fd7d19afe559
Frankie Gallina-Jones <frankieg@vmware.com>
2022-08-16 10:38:10 -0400

    address review comments
    use new packit debug logger, move config struct into its own file

--- build.go
+++ build.go
@@ -21,34 +21,6 @@ type SBOMGenerator interface {
 	Generate(path string) (sbom.SBOM, error)
 }
 
-// Configuration enumerates the environment variable configuration options
-// that govern the buildpack's behaviour.
-type Configuration struct {
-
-	// When BP_DEBUG_ENABLED=TRUE, the buildpack will include the Visual Studio
-	// Debugger in the app launch image Remote debuggers can invoke vsdbg inside
-	// the running app container and attach to vsdbg's exposed port; also, the
-	// buildpack will set ASPNETCORE_ENVIRONMENT=Development.
-	DebugEnabled bool `env:"BP_DEBUG_ENABLED"`
-
-	// When BP_LIVE_RELOAD_ENABLED=TRUE, the buildpack will make the app's entrypoint
-	// process reload on changes to program files in the app container. It will
-	// include watchexec in the app launch image and make the default container
-	// entrypoint watchexec + <the usual app entrypoint>. See
-	// https://github.com/watchexec/watchexec for more on watchexec as a
-	// reloadable process manager.
-	LiveReloadEnabled bool `env:"BP_LIVE_RELOAD_ENABLED"`
-
-	// BP_LOG_LEVEL determines the amount of logs produced by the buildpack. Set
-	// BP_LOG_LEVEL=DEBUG for more detailed logs.
-	LogLevel string `env:"BP_LOG_LEVEL,default=INFO"`
-
-	// When BP_DOTNET_PROJECT_PATH is set to a relative path, the buildpack
-	// will look for project file(s) in that subdirectory to determine which
-	// project to build into the app container.
-	ProjectPath string `env:"BP_DOTNET_PROJECT_PATH"`
-}
-
 // Build will return a packit.BuildFunc that will be invoked during the build
 // phase of the buildpack lifecycle.
 //
@@ -182,12 +154,7 @@ func Build(
 			portChooserLayer.LaunchEnv.Default("ASPNETCORE_ENVIRONMENT", "Development")
 		}
 
-		// logger.LayerFlags(portChooserLayer)
-		logger.Debug.Process("Setting up layer '%s'", portChooserLayer.Name)
-		logger.Debug.Subprocess("Available at app launch: %t", portChooserLayer.Launch)
-		logger.Debug.Subprocess("Available to other buildpacks: %t", portChooserLayer.Build)
-		logger.Debug.Subprocess("Cached for rebuilds: %t", portChooserLayer.Cache)
-		logger.Debug.Break()
+		logger.LayerFlags(portChooserLayer)
 		logger.EnvironmentVariables(portChooserLayer)
 
 		return packit.BuildResult{
