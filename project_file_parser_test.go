f3a7dda7ee483317d8bcf9f472a8db9899afffc0
Brynley McDonald <brynley+github@zephire.nz>
2025-11-29 18:04:32 +1300

    Fix stricter linting errors (#824)

--- project_file_parser_test.go
+++ project_file_parser_test.go
@@ -91,7 +91,10 @@ func testProjectFileParser(t *testing.T, context spec.G, it spec.S) {
 		it.Before(func() {
 			file, err := os.CreateTemp("", "app.csproj")
 			Expect(err).NotTo(HaveOccurred())
-			defer file.Close()
+			defer func() {
+				err = file.Close()
+				Expect(err).NotTo(HaveOccurred())
+			}()
 
 			path = file.Name()
 		})
@@ -198,7 +201,10 @@ func testProjectFileParser(t *testing.T, context spec.G, it spec.S) {
 		it.Before(func() {
 			file, err := os.CreateTemp("", "app.csproj")
 			Expect(err).NotTo(HaveOccurred())
-			defer file.Close()
+			defer func() {
+				err = file.Close()
+				Expect(err).NotTo(HaveOccurred())
+			}()
 
 			path = file.Name()
 		})
